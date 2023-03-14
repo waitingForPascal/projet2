f310804badccb201e30272ec3df5fd6046b3400e
HamiOmrab <apprendrefran43@gmail.com>
2023-03-14 12:33:24 -0400

    test

--- resources/views/ajout.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div id="ajout"></div>
-@endsection
-
-@section('scripts')
-    <script src="{{ asset('js/app.js') }}"></script>
-@endsection
--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-let mix = require("laravel-mix");
-
-mix.react("resources/js/components/Example.js", "public/js");
-
-// mix.react();
