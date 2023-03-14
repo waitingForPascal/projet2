34e94847b5da6399bb12c5781e7df6205c9492dc
Hangfei Lin <1023584582@qq.com>
2023-03-13 21:47:11 -0400

    modification de bouteille

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BouteilleController ;
 use App\Http\Controllers\CellierController ;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,16 +14,14 @@ use App\Http\Controllers\CellierController ;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// -----------------------test
-
-
-Route::get('/react', function () {
-    return view('react');
-});
+// ------------------------------test
+Route::get('/aaa', [BouteilleController::class, 'modifierUnBouteille']);
 
 Route::get('/test', function () {
     return view('test');
 });
+// ------------------------------test
+
 
 
 Auth::routes();
@@ -38,8 +37,12 @@ Route::get('/', [CellierController::class, 'index']);
 Route::get('/home', [CellierController::class, 'index']);
 Route::get('/accueil', [CellierController::class, 'index'])->name('accueil');
 
-
+// récupérer tous les bouteilles
 Route::get('/getListeBouteilleCellier', [BouteilleController::class, 'getListeBouteilleCellier']);
+// modificaiton la quantité de bouteille dans un cellier
 Route::patch('/bouteille/{id}', [BouteilleController::class, 'update']);
+
+Route::patch('/modBouteille/{id}', [BouteilleController::class, 'modifierUnBouteille']);
+
 
 
